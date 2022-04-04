6c3cded7a3a236622497847959854b903dc2f0a0
Andrey Hernandez <andreyhernandeztorres@gmail.com>
2022-04-03 19:19:13 -0500

    ReadyUI

---
+++ lib/src/ui/search_page.dart
@@ -0,0 +1,184 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+//import 'package:intl/intl.dart';
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      resizeToAvoidBottomInset: false,
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 60, left: 20, right: 20),
+            child: TextField(
+              onChanged: (value) {},
+              style: TextStyle(color: Colors.white),
+              textInputAction: TextInputAction.search,
+              onSubmitted: (value) {},
+              decoration: InputDecoration(
+                  isDense: true,
+                  contentPadding: EdgeInsets.all(10),
+                  prefix: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  ),
+                  hintStyle: TextStyle(color: Colors.white),
+                  hintText: 'Buscar',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                      borderSide: BorderSide(color: Colors.white)),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                      borderSide: BorderSide(color: Colors.white))),
+            ),
+          ),
+          Divider(
+            height: 30,
+            color: Colors.white,
+          ),
+          SizedBox(
+            width: 350,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0)),
+              elevation: 5,
+              color: Colors.grey[800],
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(top: 15, left: 20, right: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Center(
+                          child: Text(
+                            'prueba'.toUpperCase(),
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Divider(
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(left: 50),
+                        child: Column(
+                          children: [
+                            Text(
+                              '27 \u2103',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 40,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              'Min: 27 \u2103 / Max: 35 \u2103',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 13),
+                            )
+                          ],
+                        ),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            padding: EdgeInsets.only(right: 20),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  padding: EdgeInsets.all(20),
+                                  width: 100,
+                                  height: 100,
+                                  color: Colors.grey,
+                                )
+                              ],
+                            ),
+                          ),
+                          Text('Tiempo actual',
+                              style: TextStyle(color: Colors.white)),
+                          SizedBox(height: 10),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+          Row(            
+            children: [
+              Padding(padding: EdgeInsets.only(top: 30, left: 10)),
+              Text('Información Adicional', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),),
+            ],
+          ),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(            
+                  children: [
+                    Text('7 km/h', style: TextStyle(color: Colors.white,  fontSize: 20),),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.air_rounded,
+                          color: Colors.white,
+                        ),
+                        Text(' Viento', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 14),),
+                      ],
+                    ),
+                    
+                  ],
+                ),
+                Column(            
+                  children: [
+                    Text('1021.5', style: TextStyle(color: Colors.white,  fontSize: 20),),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.show_chart_rounded,
+                          color: Colors.white,
+                        ),
+                        Text(' Presión del aire', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 14),),
+                      ],
+                    ),
+                  ],
+                ),Column(            
+                  children: [
+                    Text('58%', style: TextStyle(color: Colors.white,  fontSize: 20),),                  
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.opacity_rounded,
+                          color: Colors.white,
+                        ),
+                        Text(' Humedad', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 14),),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
